a077afe5e49b5ec3056754e85bd531cdea17fa55
luanesouza <luanesouza411@gmail.com>
2020-02-22 17:50:10 -0500

    user only able to update one specific person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -33,6 +33,19 @@ class PeopleController < ApplicationController
     end
   end
 
+  def update
+    unless @person[:id] == 11
+      render json: { message: "What are you updating me for? My info is all correct!" }
+    else
+      if params[:person][:image_url]
+        @person.update(person_params[:image_url])
+        render json: { message: "Thank you! Hope you got the correct picture. Here, update my image to this one: https://bloximages.chicago2.vip.townnews.com/trib.com/content/tncms/assets/v3/editorial/2/39/239d5d03-3ba8-573d-b18b-ef245dd0599a/5d0998cdc4c34.image.jpg" }
+      else
+        render json: { message: 'Hm... looks like you chose the right person, but are trying to update the wrong attribute' }
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_person
@@ -41,6 +54,6 @@ class PeopleController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def person_params
-      params.require(:person).permit(:name, :bio, :dob)
+      params.require(:person).permit(:name, :bio, :dob, :image_url)
     end
 end
